9874640cbe34ebe1dbd18111e1fa525e60d9477d
Zeeger Lubsen <zeeger@lubsen.eu>
2016-12-11 17:10:59 +0100

    Nodes are now able to form a cluster.

--- src/main/java/eu/lubsen/StoredValueAccounts/Node.java
+++ src/main/java/eu/lubsen/StoredValueAccounts/Node.java
@@ -10,12 +10,16 @@ import io.vertx.ext.web.handler.BodyHandler;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
 import eu.lubsen.entities.Account;
 import eu.lubsen.entities.Transfer;
 import eu.lubsen.entities.TransferStatus;
 
 public class Node extends AbstractVerticle {
 
+	private HazelcastInstance hz;
 	private Controller controller;
 	private String nodeName;
 	private boolean statusRunning = false;
@@ -23,26 +27,36 @@ public class Node extends AbstractVerticle {
 	@Override
 	public void start(Future<Void> fut) {
 		this.nodeName = config().getString("node.name", "defaultNode");
+		setupCluster();
 		setupManagers();
 
 		Router router = Router.router(vertx);
-		setupRoutes(router, fut);
+		setupRoutes(router);
 		vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("http.port", 8080),
 				result -> {
 					if (result.succeeded()) {
 						fut.complete();
-						statusRunning = true;
 					} else {
 						fut.fail(result.cause());
 					}
 				});
+	}
+	
+	private void setupCluster() {
+		vertx.executeBlocking(future -> {
+			HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+			future.complete(instance);
+		}, res -> {
+			hz = (HazelcastInstance) res.result();
+			statusRunning = true;
+		});
 	}
 
 	private void setupManagers() {
 		this.controller = new Controller();
 	}
 
-	private void setupRoutes(Router router, Future<Void> fut) {
+	private void setupRoutes(Router router) {
 		router.route().handler(BodyHandler.create());
 
 		router.post("/account").handler(this::handleAddAccount);
@@ -216,9 +230,13 @@ public class Node extends AbstractVerticle {
 	}
 
 	private void handleHealth(RoutingContext routingContext) {
+		JsonObject status = new JsonObject();
+		status.put("nodeName", this.nodeName);
+		status.put("hz-instance", this.hz.toString());
+		HttpServerResponse response = routingContext.response();
 		if(statusRunning)
-			routingContext.response().setStatusCode(200).end();
+			response.setStatusCode(200).end(status.encodePrettily());
 		else
-			routingContext.response().setStatusCode(503).end();
+			response.setStatusCode(503).end("Node not available, cluster not running (yet).");
 	}
 }
